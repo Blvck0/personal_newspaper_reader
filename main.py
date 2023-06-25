f2b98a785348173f73f74224b2039a7f42ede757
kaiser olajuwon <kaiserjaey@gmail.com>
2023-06-25 02:30:51 +0100

    assigned the api to a variable & imported the requests module

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pyttsx3
+import requests
 
 engine = pyttsx3.init('sapi5')
 
 voices = engine.getProperty('voices')
-print(voices)
-engine.setProperty('voices', voices[0].id)
+# print(voices)
+engine.setProperty('voice',voices[1].id)
+# print(voices[0].id)
+
 def speak(audio):
-    pass
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__ == '__main__':
+    speak('Hello, Welcome to my channel')
+
+    url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6dac729af57e4091ae76f578973e6085"
+
+
+
+
+
+# 6dac729af57e4091ae76f578973e6085
